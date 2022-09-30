b30081b712728a39a64c9f6f44a4f7770e74284b
JuanDeossa <juandeossa.nc@gmail.com>
2022-09-30 16:05:55 -0500

    Get elements to DOM

--- main.js
+++ main.js
@@ -8,30 +8,12 @@ const API_URLs = {
     }
 }
 
-const randomBtn = document.getElementById("myButton").addEventListener("click",f1)
+const randomBtn = document.getElementById("myButton").addEventListener("click",render)
+const cardsCointainer = document.getElementById("cards-container")
+const favsCointainer = document.getElementById("favs-container")
 const image1 = document.getElementById("img1")
-const image2 = document.getElementById("img2")
-const image3= document.getElementById("img3")
-const image4 = document.getElementById("img4")
 
-async function f1() {
-    const path = API;
-    try{
-        const response = await fetch(path)
-        const data = await response.json()
-        console.log(data);
-        const url1 = data[1-1].url
-        const url2 = data[2-1].url
-        const url3 = data[3-1].url
-        const url4 = data[4-1].url
-        image1.setAttribute("src",url1)
-        image2.setAttribute("src",url2)
-        image3.setAttribute("src",url3)
-        image4.setAttribute("src",url4)
-    }catch(error){
-        throw new Error("API Not Found")
-    }
-}
+
 
 
 
