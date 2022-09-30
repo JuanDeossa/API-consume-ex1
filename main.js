8d4fa9c26708c6f892733ec8efe7db7907973401
JuanDeossa <juandeossa.nc@gmail.com>
2022-09-30 17:20:50 -0500

    gallery

--- main.js
+++ main.js
@@ -1,12 +1,76 @@
 const API_URLs = {
     key:"api_key=live_WvEeNCvl4DkeZ2DHgp6AWjjO6m4M7dHbLTkEHPpJ8wQxReVZito9YxO80008hu62",
     base:"https://api.thecatapi.com/v1",
-    getImages:"https://api.thecatapi.com/v1/images/search?limit=4",
+    getInitialImages:"https://api.thecatapi.com/v1/images/search?limit=10",
+    getSingleImage:"https://api.thecatapi.com/v1/images/search?limit=1",
     getAddFavPath:"https://api.thecatapi.com/v1/favourites?limit=&api_key=live_WvEeNCvl4DkeZ2DHgp6AWjjO6m4M7dHbLTkEHPpJ8wQxReVZito9YxO80008hu62",
     delFavPath:(id)=>{
         return `https://api.thecatapi.com/v1/favourites/${id}?api_key=live_WvEeNCvl4DkeZ2DHgp6AWjjO6m4M7dHbLTkEHPpJ8wQxReVZito9YxO80008hu62`
     }
 }
+
+const gallery = [
+    {
+      "id": "255",
+      "url": "https://cdn2.thecatapi.com/images/255.jpg",
+      "width": 449,
+      "height": 600
+    },
+    {
+      "id": "2a4",
+      "url": "https://cdn2.thecatapi.com/images/2a4.jpg",
+      "width": 300,
+      "height": 400
+    },
+    {
+      "id": "2n9",
+      "url": "https://cdn2.thecatapi.com/images/2n9.jpg",
+      "width": 500,
+      "height": 378
+    },
+    {
+      "id": "dmd",
+      "url": "https://cdn2.thecatapi.com/images/dmd.jpg",
+      "width": 500,
+      "height": 335
+    },
+    {
+      "id": "duc",
+      "url": "https://cdn2.thecatapi.com/images/duc.jpg",
+      "width": 604,
+      "height": 453
+    },
+    {
+      "id": "MTU4ODMyNg",
+      "url": "https://cdn2.thecatapi.com/images/MTU4ODMyNg.png",
+      "width": 768,
+      "height": 1024
+    },
+    {
+      "id": "MTY0MzUxMw",
+      "url": "https://cdn2.thecatapi.com/images/MTY0MzUxMw.jpg",
+      "width": 500,
+      "height": 667
+    },
+    {
+      "id": "MTY3MDk1MQ",
+      "url": "https://cdn2.thecatapi.com/images/MTY3MDk1MQ.jpg",
+      "width": 1024,
+      "height": 768
+    },
+    {
+      "id": "tp0nHtyze",
+      "url": "https://cdn2.thecatapi.com/images/tp0nHtyze.jpg",
+      "width": 1200,
+      "height": 800
+    },
+    {
+      "id": "ZV_BdSFkA",
+      "url": "https://cdn2.thecatapi.com/images/ZV_BdSFkA.png",
+      "width": 800,
+      "height": 534
+    }
+  ]
 
 const cardsCointainer = document.getElementById("cards-container")
 const favsCointainer = document.getElementById("favs-container")
@@ -63,7 +127,7 @@ async function renderFavourites(path) {
             <div class="card">
                 <p>Favourite cat ${index+1}</p>
                 <img id="img1" src="${element.image.url}" alt="Cat Image">
-                <button class="btn del-btn" data-id="${element.id}">Delete from favourites</button>
+                <button class="btn del-btn" data-id="${element.id}">Delete</button>
             </div>
             `
         });
@@ -118,7 +182,7 @@ async function delFavItem(path) {
 }
 
 async function start() {
-    await initialRandomRender(API_URLs.getImages)
+    await initialRandomRender(API_URLs.getInitialImages)
     await renderFavourites(API_URLs.getAddFavPath)
 }
 
