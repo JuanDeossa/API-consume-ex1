b9700e6f138d59d4e8a172d78d2ef6456f38fec2
JuanDeossa <juandeossa.nc@gmail.com>
2022-09-30 16:03:21 -0500

    url Object

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-const API = "https://api.thecatapi.com/v1/images/search?limit=4"
-const API_key = "live_WvEeNCvl4DkeZ2DHgp6AWjjO6m4M7dHbLTkEHPpJ8wQxReVZito9YxO80008hu62"
+const API_URLs = {
+    key:"api_key=live_WvEeNCvl4DkeZ2DHgp6AWjjO6m4M7dHbLTkEHPpJ8wQxReVZito9YxO80008hu62",
+    base:"https://api.thecatapi.com/v1",
+    getImages:"https://api.thecatapi.com/v1/images/search?limit=4",
+    getAddFavPath:"https://api.thecatapi.com/v1/favourites?limit=&api_key=live_WvEeNCvl4DkeZ2DHgp6AWjjO6m4M7dHbLTkEHPpJ8wQxReVZito9YxO80008hu62",
+    delFavPath:(id)=>{
+        return `https://api.thecatapi.com/v1/favourites/${id}?api_key=live_WvEeNCvl4DkeZ2DHgp6AWjjO6m4M7dHbLTkEHPpJ8wQxReVZito9YxO80008hu62`
+    }
+}
 
 const randomBtn = document.getElementById("myButton").addEventListener("click",f1)
 const image1 = document.getElementById("img1")
@@ -25,6 +32,10 @@ async function f1() {
         throw new Error("API Not Found")
     }
 }
+
+
+
+
 
 
 
